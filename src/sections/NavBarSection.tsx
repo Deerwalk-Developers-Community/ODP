38e7d7af625cf42209a2a3417b68df179d107fa4
chrisdvkta <chrisdevkota77@gmail.com>
2024-10-22 23:06:12 +0545

    fix:Journey Section width issue

--- src/sections/NavBarSection.tsx
+++ src/sections/NavBarSection.tsx
@@ -4,41 +4,41 @@ import Hamburger from "../../public/images/hamburger";
 import NavbarCard from "../components/NavbarCard";
 
 export default function NavBarSection() {
-    const [isNavbarCardVisible, setNavbarCardVisible] = useState(false);
+  const [isNavbarCardVisible, setNavbarCardVisible] = useState(false);
 
-    const toggleNavbarCard = () => {
-        console.log("Toggle button clicked!");
-        setNavbarCardVisible(!isNavbarCardVisible);
-    };
+  const toggleNavbarCard = () => {
+    console.log("Toggle button clicked!");
+    setNavbarCardVisible(!isNavbarCardVisible);
+  };
 
-    useEffect(() => {
-        console.log("NavbarCard visibility changed:", isNavbarCardVisible);
-    }, [isNavbarCardVisible]);
+  useEffect(() => {
+    console.log("NavbarCard visibility changed:", isNavbarCardVisible);
+  }, [isNavbarCardVisible]);
 
-    return (
-        <div className="bg-[#1C2D27] w-full h-[6rem] fixed top-0 z-10 flex flex-row lg:justify-between justify-end px-10">
-            <div className="lg:block hidden">
-                <Logo width="75px" />
-            </div>
-            <div className="flex lg:gap-32 gap-4 text-white font-sans justify-center items-center text-lg lg:text-xl">
-                <div className="h-full w-[5rem] flex justify-center items-center">
-                    Events
-                </div>
-                <div className="h-full w-[5rem] flex justify-center items-center">
-                    Gallery
-                </div>
-                <div
-                    className="h-full w-[5rem] flex justify-center items-center cursor-pointer"
-                    onClick={toggleNavbarCard}
-                >
-                    <Hamburger />
-                </div>
-            </div>
-            {isNavbarCardVisible && (
-                <div className="absolute top-[6rem] left-0 w-full">
-                    <NavbarCard />
-                </div>
-            )}
+  return (
+    <div className="bg-[#1C2D27] w-full h-[6rem] fixed top-0 z-10 flex flex-row lg:justify-between justify-end px-10">
+      <div className="lg:block hidden">
+        <Logo width="75px" />
+      </div>
+      <div className="flex lg:gap-32 gap-4 text-white font-sans justify-center items-center text-lg lg:text-xl">
+        <div className="h-full w-[5rem] flex justify-center items-center">
+          Events
         </div>
-    );
+        <div className="h-full w-[5rem] flex justify-center items-center">
+          Gallery
+        </div>
+        <div
+          className="h-full  flex justify-center items-center cursor-pointer"
+          onClick={toggleNavbarCard}
+        >
+          <Hamburger />
+        </div>
+      </div>
+      {isNavbarCardVisible && (
+        <div className="absolute top-[6rem] left-0 w-full">
+          <NavbarCard />
+        </div>
+      )}
+    </div>
+  );
 }
